865f6e67dc94da42eb5113fcfb657a7c55bc2a83
Sergej Yakushenok <sergej_yakushenok@epam.com>
2019-07-21 18:09:16 +0300

    add router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import router from './routes'
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 Vue.http.options.root = 'https://jsonplaceholder.typicode.com/'
 
-Vue.config.productionTip = false
-
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router
+})
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router'
+import App from './App.vue'
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: App
+        }
+    ]
+})
